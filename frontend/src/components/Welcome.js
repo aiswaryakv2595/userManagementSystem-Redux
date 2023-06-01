2b44c37f4c63fb23d17980104aa42cf51803d4e0
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-06-01 12:31:11 +0530

    updated

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
+const Welcome = () => {
+  const [user, setUser] = useState(null);
+
+  const sendRequest = async () => {
+    try {
+      const res = await axios.get('http://localhost:5000/api/user', {
+        withCredentials: true
+      });
+      const data = res.data; // Perform null check before accessing data property
+      console.log('data==>',data)
+      return data;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    sendRequest().then(data => {
+      if (data) {
+        setUser(data.user);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      {user && <h1>Welcome, {user.name}!</h1>}
+      {/* Render the user name if the user data is available */}
+    </div>
+  );
+};
+
+export default Welcome;
